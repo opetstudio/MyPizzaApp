1fec53203a021f6997cc662660bd7ca63732a67b
Nofrets Poai <opetstudio@gmail.com>
2019-06-26 23:00:30 +0700

    unmark this line to run storybook

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
+// import App from './storybook'
 
 AppRegistry.registerComponent('PizzaApp', () => App)
