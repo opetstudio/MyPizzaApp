0af7acc46f4525e0431670005e29af256d0a4cc4
Nofrets Poai <opetstudio@gmail.com>
2019-06-28 11:47:22 +0700

    generate login screen

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,17 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
+import LoginScreen from '../Containers/LoginScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator({
+  LoginScreen: { screen: LoginScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'LoginScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
