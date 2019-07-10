3c6fb2a8921829df849f3179a168aa9e60870459
Nofrets Poai <opetstudio@gmail.com>
2019-07-10 16:19:16 +0700

    dev screen, adding some fonts

--- android/app/src/main/java/com/pizzaapp/MainActivity.java
+++ android/app/src/main/java/com/pizzaapp/MainActivity.java
@@ -13,7 +13,7 @@ import org.devio.rn.splashscreen.SplashScreen; // here
 public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);  // here
+//        SplashScreen.show(this);  // here
         super.onCreate(savedInstanceState);
     }
 
