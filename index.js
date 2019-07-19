39bcc1dfb91b219b6efedc37109d4220cba3178f
Nofrets Poai <opetstudio@gmail.com>
2019-07-19 07:29:35 +0700

    rm App2, switch to App

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
-// import App from './App/Containers/App'
-import App from './App2/Containers/App'
+import App from './App/Containers/App'
+// import App from './App2/Containers/App'
 // import App from './storybook'
 
 AppRegistry.registerComponent('PizzaApp', () => App)
