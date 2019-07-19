7083e8f3c7ab374b6cb69491e2544e2e90655a55
Nofrets Poai <opetstudio@gmail.com>
2019-07-19 10:27:09 +0700

    dev app

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
-import App from './SkeletonApp/Containers/App'
+import App from './Bayar2App/Containers/App'
+// import App from './SkeletonApp/Containers/App'
 // import App from './App/Containers/App'
 // import App from './App2/Containers/App'
 // import App from './storybook'
