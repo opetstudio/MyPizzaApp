ce49ce14f58aef383c913e8646c7463f557b5bcb
Nofrets Poai <opetstudio@gmail.com>
2019-07-25 16:14:26 +0700

    safe area

--- Bayar2App/Components/SafeArea/index.js
+++ Bayar2App/Components/SafeArea/index.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
-import {SafeAreaView, View} from 'react-navigation'
+import {SafeAreaView} from 'react-navigation'
+import {View} from 'react-native'
 import PropTypes from 'prop-types'
 
 import styles from './styles'
 
 export default class SafeArea extends Component {
   static propTypes = {
-    navigator: PropTypes.node
+    children: PropTypes.node
   }
   render () {
     return (
       <SafeAreaView style={{ flex: 1 }} forceInset={{ bottom: 'never' }}>
-        {this.props.navigator}
+        {this.props.children}
         {<View style={styles.fixBackgroundTop} />}
       </SafeAreaView>
     )
