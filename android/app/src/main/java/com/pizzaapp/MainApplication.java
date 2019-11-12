ae4705a6fb081b095f4ec5428f88383b408a14e6
Nofrets Poai <opetstudio@gmail.com>
2019-11-12 12:52:03 +0700

    temporary

--- android/app/src/main/java/com/pizzaapp/MainApplication.java
+++ android/app/src/main/java/com/pizzaapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.CallbackManager;
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
@@ -62,6 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
+            new RNCameraPackage(),
             new OrientationPackage(),
             new VectorIconsPackage(),
                 new SvgPackage(),
